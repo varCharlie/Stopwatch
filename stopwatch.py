6a158aca40a7d5397013a36ddbe709bcadeaf3a5
Charles Pantoga <charles.pantoga@wellspring.com>
2016-06-03 11:45:11 -0500

    Made some fixes to cleanup the code

--- stopwatch.py
+++ stopwatch.py
@@ -6,19 +6,22 @@
 
 import time
 
-# Stopwatch Exception Classes
 class StopwatchError(Exception):
+    '''Stopwatch Base Error Class'''
     pass
 
 class InvalidStateError(StopwatchError):
+    '''Invalid State Base Error Class'''
     pass
 
 class NotRunningError(InvalidStateError):
+    '''Stopwatch Is Not Running Error'''
     def __init__(self):
         msg = "Invalid State: Expected Stopwatch to be running."
         super().__init__(msg)
 
 class NotStoppedError(InvalidStateError):
+    '''Stopwatch Is Not Stopped Error'''
     def __init__(self, caller):
         msg = "Invalid State: Expected Stopwatch to be stopped."
         super().__init__(msg)
@@ -30,40 +33,56 @@ class Stopwatch:
     def __init__(self, *, precision=None):
         self.__is_running = False
         self.__times = []
+        if isinstance(precision, float):
+            precision = str(precision).split('.')[0]
+        if isinstance(precision, int):
+            precision = str(precision)
         self.__precision = precision
 
+
     def start(self):
+        '''Start the stopwatch if it's not already running'''
         if self.__is_running:
             raise NotStoppedError
         self.__is_running = True
         self.__times.append({
-            'start': time.time(),
-            'stop': None,
-            })
+                'start': time.time(),
+                'stop': None,
+                })
 
     def stop(self):
+        '''Stop the stopwatch if it's running'''
         if not self.__is_running:
             raise NotRunningError
         self.__times[-1]['stop'] = time.time()
         self.__is_running = False
 
     def reset(self):
+        '''Reset the stopwatch if it's not running'''
         if self.__is_running:
             raise NotStoppedError
         self.__times = []
 
     def total(self):
+        '''Calculate total time tracked if stopwatch is stopped'''
+        if self.__is_running:
+            raise NotStoppedError
         total = sum(t['stop']-t['start'] for t in self.__times)
         if self.__precision:
-            return float(str('{:.' + str(self.__precision) + 'f}').format(total))
+            return float(('{:.' + self.__precision + 'f}').format(total))
         else:
             return total
 
-    def status(self):
+    def status(self, *, typename=str):
+        output_map = {
+                str: ('running', 'stopped'),
+                bool: (True, False),
+                int: (1, 0),
+                }
         if self.__is_running:
-            return "running"
+            return output_map[typename][0]
         else:
-            return "stopped"
+            return output_map[typename][1]
 
 
 # Interactive Stopwatch Program
@@ -73,34 +92,29 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--precision', required=False, default=None,
-                        help='Number of decimal places displayed.')
+                        choices=range(17), help='Floating pt precision limit.')
     args = parser.parse_args()
-    print(args.precision)
-
-    print("Stopwatch Program\n====================\n\n")
 
     watch = Stopwatch(precision=args.precision)
 
     actions = {
-        'status': watch.status,
-        'start': watch.start,
-        'reset': watch.reset,
-        'total': watch.total,
-        'stop': watch.stop,
-        'quit': sys.exit,
-        }
-
+            'status': watch.status,
+            'start': watch.start,
+            'reset': watch.reset,
+            'total': watch.total,
+            'stop': watch.stop,
+            'quit': sys.exit,
+            }
     action_output = {
-        'start': 'Stopwatch is now running...',
-        'reset': 'Stopwatch timer has been reset...',
-        'stop': 'Stopwatch is stopped...',
-        'quit': 'Thank you, goodbye...',
-        }
-
+            'start': 'Stopwatch is now running...',
+            'reset': 'Stopwatch timer has been reset...',
+            'stop': 'Stopwatch is stopped...',
+            'quit': 'Thank you, goodbye...',
+            }
     property_output = {
-        'total': 'Calculating total...',
-        'status': 'Checking status...',
-        }
+            'total': 'Calculating total...',
+            'status': 'Checking status...',
+            }
 
     def choices():
         return '''\
@@ -130,4 +144,4 @@ What would you like to do? '''
     except KeyboardInterrupt:
         print()
         print("Goodbye!")
-        sys.exit(-1)
+        sys.exit()
